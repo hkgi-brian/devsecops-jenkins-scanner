5dc305177e4e49536de02fee0a8d22022e2bb11b
EC2 Default User <ec2-user@ip-172-31-35-30.ap-southeast-1.compute.internal>
2024-04-26 07:41:18 +0000

    update
    config the codebuild name

---
+++ setup_aws.sh
@@ -0,0 +1,7 @@
+export AWS_PAGER= ;\
+	export AWS_REGION=ap-southeast-1 ;\
+	export ACCOUNT=$(aws sts get-caller-identity --out json --query 'Account' | sed 's/"//g') ;\
+	export CDK_DEFAULT_ACCOUNT=$ACCOUNT ;\
+	export CDK_DEFAULT_REGION=$AWS_REGION ;\
+	export CURRENT_IP=54.179.130.159 ;\
+	echo -e "$ACCOUNT \\n$CURRENT_IP" ;
